bda11ab98ffc127edc61b95351d3968932c891be
David Caspers <caspersdavid@gmail.com>
2025-04-23 14:52:39 -0500

    Updating progressing in hand-coding llm

--- embeder.py
+++ embeder.py
@@ -2,6 +2,8 @@ import urllib.request
 
 url = "https://raw.githubusercontent.com/rasbt/LLMs-from-scratch/main/ch02/01_main-chapter-code/the-verdict.txt"
 
+file_path = "the-verdict.txt"
 
-resp = urllib.request.urlopen(url)
-print(resp.read())
+resp = urllib.request.urlretrieve(url)
+txt = resp.read()
+
